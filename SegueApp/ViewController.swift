9c2ce8593715dc7ac046caea9452639ae8a55ccf
Felipe Augusto <augusto_felipe@icloud.com>
2022-08-01 13:35:15 -0300

    Initial Commit

---
+++ SegueApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegueApp
+//
+//  Created by Felipe Augusto Correia on 01/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
